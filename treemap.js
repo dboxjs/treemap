0166a2dea5d21eeba94d9e87f11e3dd335e9b03c
Blanca Azucena López Garduño <blanca.lopez@data4.mx>
2018-04-04 14:25:20 -0500

    Remove class in tooltip

--- treemap.js
+++ treemap.js
@@ -13,7 +13,7 @@ export default function(config, helper) {
     vm._config._format = d3.format(",.1f");
     vm._config._labels = false;
     vm._config.tip = function(d) {
-      var html = '<div class="d3-tip">';
+      var html = '<div>';
       html += '<span>' + d.data.name + "</span><br><span>" + vm._config._format(d.value) + '</span>';
       html += '</div>';
       return html;
